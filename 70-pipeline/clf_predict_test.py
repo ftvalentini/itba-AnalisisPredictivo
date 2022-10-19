9fe9feb08f1e2e0d072b4e627ad9da76015ce7b9
ftvalentini <ft.valentini@gmail.com>
2022-10-19 15:18:32 -0300

    update clase pipeline

--- 70-pipeline/clf_predict_test.py
+++ 70-pipeline/clf_predict_test.py
@@ -16,10 +16,10 @@ df_test = pd.read_csv("data_test.csv", index_col=0)
 
 logging.info('Predicting...')
 X_test = df_test.drop(["target"], axis=1)
-y_test = pipe.predict_proba(X_test)[:, 1]
+y_pred = pipe.predict_proba(X_test)[:, 1]
 
 logging.info('Saving predictions...')
-df_out = pd.DataFrame({"id": X_test.index, "y_pred": y_test})
+df_out = pd.DataFrame({"id": X_test.index, "y_pred": y_pred})
 df_out.sort_values("id", inplace=True)
 print(df_out.head())
 # df_out.to_csv("predictions.csv", index=False)
